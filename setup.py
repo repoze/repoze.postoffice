27cd81cca83b76e59b94dd6c45a278e4b06dabba
Chris Rossi <chris@christophermrossi.com>
2010-07-30 19:52:33 +0000

    Prep for release.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@
 #
 ##############################################################################
 
-__version__ = '0.4dev'
+__version__ = '0.4'
 
 import os
 from setuptools import setup, find_packages
