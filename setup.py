b26a48425510451676e88f1721b1c7565476c2eb
Chris Rossi <chris@christophermrossi.com>
2011-04-15 19:13:26 +0000

    Back to dev.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@
 #
 ##############################################################################
 
-__version__ = '0.9'
+__version__ = '0.10dev'
 
 import os
 from setuptools import setup, find_packages
