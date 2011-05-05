7139bea0b150a3278a827db1d3c8e09e1ffa8307
Robert Marianski <rob@marianski.com>
2011-05-05 21:08:33 +0000

    prep for release

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@
 #
 ##############################################################################
 
-__version__ = '0.13dev'
+__version__ = '0.13'
 
 import os
 from setuptools import setup, find_packages
