ef929441ba53cf9c5019569bd3036c4c4f28be0c
Chris Rossi <chris@christophermrossi.com>
2012-05-14 17:54:54 +0000

    Bump version.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@
 #
 ##############################################################################
 
-__version__ = '0.19'
+__version__ = '0.20'
 
 import os
 from setuptools import setup, find_packages
